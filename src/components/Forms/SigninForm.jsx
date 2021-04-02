d91fcbfa9f3699411f4f7c5974753b1d335fc981
OrlandoMendoza17 <ommv.17@gmail.com>
2021-04-02 01:34:09 -0200

    Responsive de Playlist y Media

--- src/components/Forms/SigninForm.jsx
+++ src/components/Forms/SigninForm.jsx
@@ -1,44 +1,40 @@
 import React from 'react';
 import Input from '@components/widgets/Input';
 import SubmitButton from '@components/widgets/SubmitButton';
-import {Link} from 'react-router-dom';
-import LoginButton from '@components/UI/LoginButton';
 
 const SigninForm = ({handleChange, handleSubmit, handleInvalid, form, children}) =>(
-  <>
-    <form className="FormatForm__Form" onSubmit={handleSubmit} onInvalid={handleInvalid}>
-      <Input 
-        className="FormatForm__Form--input" 
-        handleChange={handleChange} 
-        type="text"
-        name="name" 
-        placeholder="Nombre" 
-        value={form.name} 
-        required={true}
-      />
-      <Input 
-        className="FormatForm__Form--input" 
-        handleChange={handleChange} 
-        type="email"
-        name="email" 
-        placeholder="Correo" 
-        value={form.email} 
-        required={true}
-      />
-      <Input 
-        className="FormatForm__Form--input" 
-        handleChange={handleChange}
-        type="password" 
-        name="password" 
-        placeholder="Contraseña" 
-        value={form.password} 
-        required={true}
-      />
-      <SubmitButton>
-        Registrarme
-      </SubmitButton>
-    </form>
-  </>
+  <form className="FormatForm__Form" onSubmit={handleSubmit} onInvalid={handleInvalid}>
+    <Input 
+      className="FormatForm__Form--input" 
+      handleChange={handleChange} 
+      type="text"
+      name="name" 
+      placeholder="Nombre" 
+      value={form.name} 
+      required={true}
+    />
+    <Input 
+      className="FormatForm__Form--input" 
+      handleChange={handleChange} 
+      type="email"
+      name="email" 
+      placeholder="Correo" 
+      value={form.email} 
+      required={true}
+    />
+    <Input 
+      className="FormatForm__Form--input" 
+      handleChange={handleChange}
+      type="password" 
+      name="password" 
+      placeholder="Contraseña" 
+      value={form.password} 
+      required={true}
+    />
+    <SubmitButton>
+      Registrarme
+    </SubmitButton>
+  </form>
 )
 
 export default SigninForm;
