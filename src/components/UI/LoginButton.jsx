c57b29c863a7b9fb082b549b31be317b364073e1
OrlandoMendoza17 <ommv.17@gmail.com>
2021-04-01 22:32:44 -0200

    Creacion de <Button/> y <LoginButton>

---
+++ src/components/UI/LoginButton.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Button from '@components/widgets/Button';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+const LoginButton = ({page}) =>(
+  <>
+    {
+      (page !== 'Login') &&
+      <Link to="/Login">
+        <Button type="bold big" >
+          Iniciar Sesión
+        </Button>
+      </Link>
+    }
+  </>
+)
+
+LoginButton.propTypes = {
+  page: PropTypes.oneOf(['Login'])
+}
+
+export default LoginButton;
