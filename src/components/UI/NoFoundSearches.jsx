49d9c1e7dd996455d6089fe251d2c997717885d7
OrlandoMendoza17 <ommv.17@gmail.com>
2022-09-11 19:40:45 -0400

    El pickaku 404 ahora se trae desde imgur

--- src/components/UI/NoFoundSearches.jsx
+++ src/components/UI/NoFoundSearches.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import pikachu from '../../assets/images/pikachu.png'
 
 const NotFoundSearches = (props) =>(
   <section className="NotFoundSearches">
     <div className="NotFoundSearches__image">
-      <img src={pikachu} alt="pikachu-sorprendido"/>
+      <img src="https://i.imgur.com/aFg4F7L.png" alt="pikachu-sorprendido"/>
     </div>
     <div className="NotFoundSearches__message">
       {props.children}
